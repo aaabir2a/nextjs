bb4cb252fd39261c25f9ad872b72e51e8f88605b
aaabir2a <aaabir2a@gmail.com>
2024-11-18 16:03:04 +0600

    1.04.19

--- AI Prompt/my-nextjs-app/components/Nav.jsx
+++ AI Prompt/my-nextjs-app/components/Nav.jsx
@@ -2,21 +2,53 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
+import { signOut } from 'next-auth';
+
 
 
 const Nav = () => {
+    const isUserLoggedIn = true;
+    const [provider,setProvider] = useState(null);
+    useEffect(()=>{
+        const setProvider = async () =>{
+            const response =await getProviders();
+            setProvider(response)
+        }
+        setProvider();
+    },[])
+
+
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
-        <Link href="/" className='flex gap-2 flex-center'></Link>
+    <nav className="flex-between w-full mb-16 pt-3">
+        <Link href="/" className="flex gap-2 flex-center">
         <Image
-        src="/public/assets/images/logo.svg"
-        alt="Promptopia Logo"
+        src="/assets/images/logo.svg"
+        alt=""
         width={30}
         height={30}
-        className='object-contain'
+        className="object-contain"
         />
-        <p className='logo-text'>AI Prompt</p>
+        <p className="logo-text">AI Prompt</p>
+        </Link>
+
+        {isUserLoggedIn ? (
+            <div className="flex gap-3 md:gap-5">
+                <Link href="/create-prompt" className="black_btn">Create Post</Link>
+                <button type="button" onClick={signOut} className="outline_btn">Sign OUt</button>
+                <Link href="/profile">
+                <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                classsName="rounded-full"
+                alt="profile"
+                />
+                </Link>
+            </div>
+        ):(
+            <>
+            </>
+        )}
 
     </nav>
   )
